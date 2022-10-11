f81efcf23606149b11d101c906a82460d00597a0
Tal <trimoni8@gmail.com>
2022-10-11 17:00:44 -0400

    fix indent

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -7,7 +7,7 @@ const Student = ({student}) => {
       <h2>{student.bio}</h2>
       {student.scores.map(score =>
         <Score key={score.date} score={score} />
-        )}
+      )}
     </>
   );
 }
